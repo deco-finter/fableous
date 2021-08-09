1fda0674cf8204ee34b3d9f15fcfc94f8ac8dff4
Danny August Ramaputra <daystram@gmail.com>
2021-08-09 21:43:51 +1000

    feat: added enums for message types and user types

---
+++ fableous-be/constants/websocket.go
@@ -0,0 +1,16 @@
+package constants
+
+const (
+	WSMessageTypePaint   = "PAINT"
+	WSMessageTypeText    = "TEXT"
+	WSMessageTypeAudio   = "AUDIO"
+	WSMessageTypeConnect = "CONNECT"
+	WSMessageTypeControl = "CONTROL"
+	WSMessageTypePing    = "PING"
+	WSMessageTypeError   = "ERROR"
+
+	WSControllerRoleCharacter  = "CHARACTER"
+	WSControllerRoleBackground = "BACKGROUND"
+	WSControllerRoleStory      = "STORY"
+	WSControllerRoleHub        = "HUB"
+)
