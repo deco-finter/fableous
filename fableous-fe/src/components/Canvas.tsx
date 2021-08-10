d4ae3393d30921e5aa7b2c6ac19c3d5bc22d52d1
Danny August Ramaputra <daystram@gmail.com>
2021-08-10 14:40:42 +1000

    refactor: added canvas scaling

--- fableous-fe/src/components/Canvas.tsx
+++ fableous-fe/src/components/Canvas.tsx
@@ -7,11 +7,14 @@ import React, {
 } from "react";
 import { ControllerRole, WSMessage, WSMessageType } from "../Data";
 
+const ASPECT_RATIO = 9 / 16;
+
 const Canvas = (props: {
   wsRef: MutableRefObject<WebSocket | undefined>;
   role: ControllerRole;
+  layer: ControllerRole;
 }) => {
-  const { role, wsRef } = props;
+  const { layer, role, wsRef } = props;
   const canvasRef = useRef<HTMLCanvasElement>(document.createElement("canvas"));
   const [lastPos, setLastPos] = useState([0, 0]);
   const [allowDrawing, setAllowDrawing] = useState(false);
@@ -22,6 +25,14 @@ const Canvas = (props: {
     return [x - bound.x, y - bound.y];
   };
 
+  const scaleDownXY = (x: number, y: number) => {
+    return [x / canvasRef.current.width, y / canvasRef.current.height];
+  };
+
+  const scaleUpXY = (x: number, y: number) => {
+    return [x * canvasRef.current.width, y * canvasRef.current.height];
+  };
+
   const drawSegment = useCallback(
     (x1: number, y1: number, x2: number, y2: number) => {
       const ctx = canvasRef.current.getContext(
@@ -30,12 +41,14 @@ const Canvas = (props: {
       ctx.moveTo(x1, y1);
       ctx.lineTo(x2, y2);
       ctx.stroke();
+      const [normX1, normY1] = scaleDownXY(x1, y1);
+      const [normX2, normY2] = scaleDownXY(x2, y2);
       if (role !== ControllerRole.Hub) {
         wsRef.current?.send(
           JSON.stringify({
             role,
             type: WSMessageType.Paint,
-            data: { x1, y1, x2, y2 },
+            data: { x1: normX1, y1: normY1, x2: normX2, y2: normY2 },
           })
         );
       }
@@ -49,12 +62,11 @@ const Canvas = (props: {
         const msg: WSMessage = JSON.parse(ev.data);
         switch (msg.type) {
           case WSMessageType.Paint:
-            drawSegment(
-              msg.data.x1 || 0,
-              msg.data.y1 || 0,
-              msg.data.x2 || 0,
-              msg.data.y2 || 0
-            );
+            if (msg.role === layer) {
+              const [x1, y1] = scaleUpXY(msg.data.x1 || 0, msg.data.y1 || 0);
+              const [x2, y2] = scaleUpXY(msg.data.x2 || 0, msg.data.y2 || 0);
+              drawSegment(x1, y1, x2, y2);
+            }
             break;
           default:
         }
@@ -62,7 +74,7 @@ const Canvas = (props: {
         console.log(e);
       }
     },
-    [drawSegment]
+    [drawSegment, layer]
   );
 
   function onMouseDown(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
@@ -95,8 +107,8 @@ const Canvas = (props: {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.width = window.innerHeight;
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetWidth * ASPECT_RATIO;
     setAllowDrawing(role !== ControllerRole.Hub);
     const ws = wsRef.current;
     ws?.addEventListener("message", readMessage);
@@ -109,7 +121,10 @@ const Canvas = (props: {
       onMouseDown={onMouseDown}
       onMouseMove={onMouseMove}
       onMouseUp={onMouseUp}
-      style={{ borderWidth: 4 }}
+      style={{
+        borderWidth: 4,
+        width: "90vw",
+      }}
     />
   );
 };
