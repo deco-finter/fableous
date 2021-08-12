eeb2cd600bee1ec6aa54ee13b9b186903381b64d
Danny August Ramaputra <daystram@gmail.com>
2021-08-12 13:52:49 +1000

    feat: added ws payload extractor

--- fableous-be/datatransfers/websocket.go
+++ fableous-be/datatransfers/websocket.go
@@ -5,3 +5,14 @@ type WSMessage struct {
 	Role string      `json:"role,omitempty"`
 	Data interface{} `json:"data"`
 }
+
+type WSMessageData struct {
+	X1    int    `json:"x1,omitempty"`
+	Y1    int    `json:"y1,omitempty"`
+	X2    int    `json:"x2,omitempty"`
+	Y2    int    `json:"y2,omitempty"`
+	ID    int    `json:"id,omitempty"`
+	Text  string `json:"text,omitempty"`
+	Color string `json:"color,omitempty"`
+	Width int    `json:"width,omitempty"`
+}
