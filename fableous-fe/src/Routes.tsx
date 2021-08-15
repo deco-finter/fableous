fa67ac87bd839e610bccf7ef7098f2143af07c00
wahyuadt <wahyuadt9@gmail.com>
2021-08-15 19:58:14 +1000

    feat: add route to login and register

--- fableous-fe/src/Routes.tsx
+++ fableous-fe/src/Routes.tsx
@@ -2,6 +2,8 @@ import { Route, Switch } from "react-router-dom";
 import ControllerCanvasPage from "./containers/ControllerCanvasPage";
 import HubCanvasPage from "./containers/HubCanvasPage";
 import HomePage from "./containers/HomePage";
+import LoginPage from "./containers/LoginPage";
+import RegisterPage from "./containers/RegisterPage";
 
 export default function Routes() {
   return (
@@ -9,6 +11,8 @@ export default function Routes() {
       <Route path="/" exact component={HomePage} />
       <Route path="/canvas" exact component={HubCanvasPage} />
       <Route path="/join" exact component={ControllerCanvasPage} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="/register" exact component={RegisterPage} />
     </Switch>
   );
 }
