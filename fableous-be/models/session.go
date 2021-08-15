e68ce02db8335f0acf9c3f2046e0b815fe717ff2
Danny August Ramaputra <daystram@gmail.com>
2021-08-15 19:58:49 +1000

    feat: initialized session model

---
+++ fableous-be/models/session.go
@@ -0,0 +1,67 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type sessionOrm struct {
+	db *gorm.DB
+}
+
+type Session struct {
+	ID string `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()" json:"-"`
+	// ClassroomID string `gorm:"foreignKey" json:"-"`  // TODO: pending Classroom API @jbogard
+	// Classroom   Classroom
+	Title       string    `gorm:"column:title;type:varchar(255);not null" json:"-"`
+	Description string    `gorm:"column:description;type:text" json:"-"`
+	Pages       int       `gorm:"column:pages;not null" json:"-"`
+	Completed   bool      `gorm:"column:completed;type:boolean;not null" json:"-"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"-"`
+	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"-"`
+}
+
+type SessionOrmer interface {
+	GetOneByID(id string) (session Session, err error)
+	GetOneOngoingByClassroomID(classroomID string) (session Session, err error)
+	GetAllCompletedByClassroomID(classroomID string) (sessions []Session, err error)
+	Insert(session Session) (id string, err error)
+	Update(session Session) (err error)
+	Delete(id string) (err error)
+}
+
+func NewSessionOrmer(db *gorm.DB) SessionOrmer {
+	_ = db.AutoMigrate(&Session{})
+	return &sessionOrm{db}
+}
+
+func (o *sessionOrm) GetOneByID(id string) (session Session, err error) {
+	result := o.db.Model(&Session{}).Where("id = ?", id).First(&session)
+	return session, result.Error
+}
+
+func (o *sessionOrm) GetOneOngoingByClassroomID(classroomID string) (session Session, err error) {
+	result := o.db.Model(&Session{}).Where("classroom_id = ? AND completed = ?", classroomID, false).First(&session)
+	return session, result.Error
+}
+
+func (o *sessionOrm) GetAllCompletedByClassroomID(classroomID string) (sessions []Session, err error) {
+	result := o.db.Model(&Session{}).Where("classroom_id = ? AND completed = ?", classroomID, true).Find(&sessions)
+	return sessions, result.Error
+}
+
+func (o *sessionOrm) Insert(session Session) (id string, err error) {
+	result := o.db.Model(&Session{}).Create(&session)
+	return session.ID, result.Error
+}
+
+func (o *sessionOrm) Update(session Session) (err error) {
+	result := o.db.Model(&Session{}).Model(&session).Updates(&session)
+	return result.Error
+}
+
+func (o *sessionOrm) Delete(id string) (err error) {
+	result := o.db.Delete(&Session{ID: id})
+	return result.Error
+}
