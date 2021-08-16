fb112925bec8f9bf05eb461015d5de3de18aae60
Danny August Ramaputra <daystram@gmail.com>
2021-08-16 20:13:14 +1000

    feat: added classroom check

--- fableous-be/controllers/session.go
+++ fableous-be/controllers/session.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 
+	"github.com/deco-finter/fableous/fableous-be/constants"
 	"github.com/deco-finter/fableous/fableous-be/datatransfers"
 	"github.com/deco-finter/fableous/fableous-be/handlers"
 )
@@ -25,23 +27,33 @@ func GETSession(c *gin.Context) {
 
 func POSTSession(c *gin.Context) {
 	var err error
-	classroomID := c.Param("classroom_id")
-	// TODO: check classroom ownership
-	if _, err = handlers.Handler.SessionGetOneOngoingByClassroomID(classroomID); err != nil {
-		if err == gorm.ErrRecordNotFound {
-			c.JSON(http.StatusBadRequest, datatransfers.Response{Error: "classroom already has ongoing session"})
-			return
-		} else {
-			c.JSON(http.StatusInternalServerError, datatransfers.Response{Error: "cannot check for ongoing session"})
-			return
-		}
+	var classroomInfo datatransfers.ClassroomInfo
+	if classroomInfo, err = handlers.Handler.ClassroomGetOneByID(c.Param("classroom_id")); err == gorm.ErrRecordNotFound {
+		c.JSON(http.StatusInternalServerError, datatransfers.Response{Error: "classroom does not exist"})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, datatransfers.Response{Error: "cannot get classroom detail"})
+		return
+	}
+	log.Println(classroomInfo.UserID)
+	log.Println(c.GetString(constants.RouterKeyUserID))
+	if classroomInfo.UserID != c.GetString(constants.RouterKeyUserID) {
+		c.JSON(http.StatusForbidden, datatransfers.Response{Error: "user does not own this classroom"})
+		return
 	}
 	var sessionInfo datatransfers.SessionInfo
+	if sessionInfo, err = handlers.Handler.SessionGetOneOngoingByClassroomID(classroomInfo.ID); sessionInfo.ID != "" {
+		c.JSON(http.StatusBadRequest, datatransfers.Response{Error: "classroom already has ongoing session"})
+		return
+	} else if err != gorm.ErrRecordNotFound {
+		c.JSON(http.StatusInternalServerError, datatransfers.Response{Error: "cannot check for ongoing session"})
+		return
+	}
 	if err = c.ShouldBind(&sessionInfo); err != nil {
 		c.JSON(http.StatusBadRequest, datatransfers.Response{Error: err.Error()})
 		return
 	}
-	sessionInfo.ClassroomID = classroomID
+	sessionInfo.ClassroomID = classroomInfo.ID
 	if sessionInfo.ID, err = handlers.Handler.SessionInsert(sessionInfo); err != nil {
 		c.JSON(http.StatusNotModified, datatransfers.Response{Error: "cannot create session"})
 		return
