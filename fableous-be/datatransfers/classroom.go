085f81800b8816b6d0623e18178ee3ad701289d4
MichaelSudirman <astradon.michael@gmail.com>
2021-08-16 15:34:06 +0700

    feat: updated classroom info and update info

--- fableous-be/datatransfers/classroom.go
+++ fableous-be/datatransfers/classroom.go
@@ -4,12 +4,8 @@ import (
 	"time"
 )
 
-type ClassroomUpdate struct {
-	Name string `json:"name" binding:"-"`
-}
-
 type ClassroomInfo struct {
 	ID        string    `json:"id" uri:"id"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" binding:"required"`
 	CreatedAt time.Time `json:"createdAt"`
 }
