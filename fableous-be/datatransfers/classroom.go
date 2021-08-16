0ff906fe624b5172112823a49b03da032bcf6c37
Danny August Ramaputra <daystram@gmail.com>
2021-08-16 20:12:42 +1000

    refactor: updated classroom df

--- fableous-be/datatransfers/classroom.go
+++ fableous-be/datatransfers/classroom.go
@@ -6,6 +6,7 @@ import (
 
 type ClassroomInfo struct {
 	ID        string    `json:"id" binding:"-"`
+	UserID    string    `json:"-" binding:"-"`
 	Name      string    `json:"name" binding:"required"`
 	CreatedAt time.Time `json:"createdAt" binding:"-"`
 }
