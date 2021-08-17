36dccae8b39c426c3ac59e75c200717d9c47f477
Danny August Ramaputra <daystram@gmail.com>
2021-08-17 21:29:15 +1000

    feat: added classroom detail page

--- fableous-fe/src/containers/ClassroomDetailPage.tsx
+++ fableous-fe/src/containers/ClassroomDetailPage.tsx
@@ -1,9 +1,48 @@
-import { Grid } from "@material-ui/core";
+import { useState } from "react";
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  Icon,
+  Typography,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import useAxios from "axios-hooks";
+import { useHistory, useParams } from "react-router-dom";
+import { restAPI } from "../Api";
+import { APIResponse, Classroom } from "../Data";
 
-export default function ClassroomDetailPage() {
+export default function ClassroomListPage() {
+  const history = useHistory();
+  const { classroomId } = useParams<{ classroomId: string }>();
+  const [editing] = useState<boolean>(false);
+  const [{ data: classroom, loading, error }] = useAxios<
+    APIResponse<Classroom>,
+    string
+  >(restAPI.classroom.get(classroomId));
+
   return (
     <Grid item xs={12}>
-      <h1>this is classroom detail</h1>
+      <Grid container>
+        <Button
+          onClick={() => history.push("/classrooms")}
+          disabled={editing}
+          startIcon={<Icon>arrow_backward</Icon>}
+        >
+          Back
+        </Button>
+      </Grid>
+      {loading && (
+        <Grid container justify="center">
+          <CircularProgress />
+        </Grid>
+      )}
+      {error && <Alert severity="error">Failed loading classrooms!</Alert>}
+      {!loading && !error && (
+        <Typography variant="h2" className="mb-4">
+          {classroom?.data?.name}
+        </Typography>
+      )}
     </Grid>
   );
 }
