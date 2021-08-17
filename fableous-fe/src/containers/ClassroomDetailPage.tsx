0e22c3c69b74b1ede5cfd013b8bad27164b0f2a3
Danny August Ramaputra <daystram@gmail.com>
2021-08-18 01:04:09 +1000

    feat: updated classroom detail page

--- fableous-fe/src/containers/ClassroomDetailPage.tsx
+++ fableous-fe/src/containers/ClassroomDetailPage.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   CircularProgress,
   Grid,
   Icon,
+  TextField,
   Typography,
 } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
@@ -15,11 +16,46 @@ import { APIResponse, Classroom } from "../Data";
 export default function ClassroomListPage() {
   const history = useHistory();
   const { classroomId } = useParams<{ classroomId: string }>();
-  const [editing] = useState<boolean>(false);
-  const [{ data: classroom, loading, error }] = useAxios<
+  const [editing, setEditing] = useState<boolean>(false);
+  const [classroom, setClassroom] = useState<Classroom>();
+  const [classroomCopy, setClassroomCopy] = useState<Classroom>();
+  const [{ loading: getLoading, error: getError }, get] = useAxios<
     APIResponse<Classroom>,
     string
-  >(restAPI.classroom.get(classroomId));
+  >(restAPI.classroom.get(classroomId), { manual: true });
+  const [, put] = useAxios<APIResponse<Classroom>, string>(
+    restAPI.classroom.update(classroomId),
+    { manual: true }
+  );
+
+  const handleEdit = () => {
+    if (editing) {
+      // TODO: validate
+      put({
+        data: {
+          name: classroom?.name,
+        },
+      })
+        .then(() => setEditing(false))
+        .catch((error) => console.error(error));
+    } else {
+      setEditing(true);
+      setClassroomCopy(classroom);
+    }
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+    setClassroom(classroomCopy);
+  };
+
+  useEffect(() => {
+    get()
+      .then((response) => {
+        setClassroom(response.data.data);
+      })
+      .catch((error) => console.error(error));
+  }, [get]);
 
   return (
     <Grid item xs={12}>
@@ -32,16 +68,50 @@ export default function ClassroomListPage() {
           Back
         </Button>
       </Grid>
-      {loading && (
+      {getLoading && (
         <Grid container justify="center">
           <CircularProgress />
         </Grid>
       )}
-      {error && <Alert severity="error">Failed loading classrooms!</Alert>}
-      {!loading && !error && (
-        <Typography variant="h2" className="mb-4">
-          {classroom?.data?.name}
-        </Typography>
+      {getError && <Alert severity="error">Failed loading classrooms!</Alert>}
+      {!getLoading && !getError && (
+        <>
+          {editing ? (
+            <TextField
+              value={classroom?.name}
+              required
+              onChange={(e) =>
+                setClassroom({
+                  ...classroom,
+                  name: e.target.value,
+                } as Classroom)
+              }
+            />
+          ) : (
+            <Typography variant="h2" className="mb-4">
+              {classroom?.name}
+            </Typography>
+          )}
+
+          <Grid item xs={12}>
+            {editing ? (
+              <>
+                <Button onClick={handleEdit} startIcon={<Icon>save</Icon>}>
+                  Save
+                </Button>
+                <Button onClick={handleCancel} startIcon={<Icon>cancel</Icon>}>
+                  Cancel
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button onClick={handleEdit} startIcon={<Icon>edit</Icon>}>
+                  Edit
+                </Button>
+              </>
+            )}
+          </Grid>
+        </>
       )}
     </Grid>
   );
