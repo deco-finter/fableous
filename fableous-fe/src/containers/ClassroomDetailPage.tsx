01a668ab11cf3a0eda50334b89585892c78f94f7
Danny August Ramaputra <daystram@gmail.com>
2021-08-18 14:08:19 +1000

    feat: added delete classroom

--- fableous-fe/src/containers/ClassroomDetailPage.tsx
+++ fableous-fe/src/containers/ClassroomDetailPage.tsx
@@ -4,14 +4,15 @@ import {
   CircularProgress,
   Grid,
   Icon,
+  IconButton,
   TextField,
   Typography,
 } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import useAxios from "axios-hooks";
-import { useHistory, useParams } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { restAPI } from "../Api";
-import { APIResponse, Classroom } from "../Data";
+import { APIResponse, Classroom, Session } from "../Data";
 
 export default function ClassroomListPage() {
   const history = useHistory();
@@ -19,19 +20,36 @@ export default function ClassroomListPage() {
   const [editing, setEditing] = useState<boolean>(false);
   const [classroom, setClassroom] = useState<Classroom>();
   const [classroomCopy, setClassroomCopy] = useState<Classroom>();
-  const [{ loading: getLoading, error: getError }, get] = useAxios<
+  const [ongoingSession, setOngoingSession] = useState<Session>();
+  const [{ loading: getLoading, error: getError }, executeGet] = useAxios<
     APIResponse<Classroom>,
     APIResponse<undefined>
   >(restAPI.classroom.getOne(classroomId), { manual: true });
-  const [, put] = useAxios<APIResponse<Classroom>, APIResponse<undefined>>(
-    restAPI.classroom.update(classroomId),
+  const [, executePut] = useAxios<
+    APIResponse<Classroom>,
+    APIResponse<undefined>
+  >(restAPI.classroom.update(classroomId), { manual: true });
+  const [{ loading: deleteLoading }, executeDelete] = useAxios<
+    APIResponse<string>,
+    APIResponse<undefined>
+  >(restAPI.classroom.delete(classroomId), { manual: true });
+  const [{ loading: getOngoingSessionLoading }, executeGetOngoingSession] =
+    useAxios<APIResponse<Session>, APIResponse<undefined>>(
+      restAPI.session.getOngoing(classroomId),
+      { manual: true }
+    );
+  const [
+    { loading: deleteOngoingSessionLoading },
+    executeDeleteOngoingSession,
+  ] = useAxios<APIResponse<string>, APIResponse<undefined>>(
+    restAPI.session.delete(classroomId, ongoingSession?.id || ""),
     { manual: true }
   );
 
   const handleEdit = () => {
     if (editing) {
       // TODO: validate
-      put({
+      executePut({
         data: {
           name: classroom?.name,
         },
@@ -49,16 +67,49 @@ export default function ClassroomListPage() {
     setClassroom(classroomCopy);
   };
 
+  const handleDelete = () => {
+    executeDelete()
+      .then(() => {
+        history.push("/classroom");
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const handleDeleteOngoingSession = () => {
+    executeDeleteOngoingSession()
+      .then(() => {
+        setOngoingSession(undefined);
+      })
+      .catch((error) => console.error(error));
+  };
+
   useEffect(() => {
-    get()
+    executeGet()
       .then((response) => {
         setClassroom(response.data.data);
       })
-      .catch((error) => console.error(error));
-  }, [get]);
+      .catch((error) => {
+        if (error.response.status === 404) {
+          history.push("/classroom");
+        } else {
+          console.error(error);
+        }
+      });
+    executeGetOngoingSession()
+      .then((response) => {
+        setOngoingSession(response.data.data);
+      })
+      .catch((error) => {
+        if (error.response?.status === 404) {
+          setOngoingSession(undefined);
+        } else {
+          console.error(error);
+        }
+      });
+  }, [executeGet, executeGetOngoingSession, history]);
 
   return (
-    <Grid item xs={12}>
+    <Grid container xs={12}>
       <Grid container>
         <Button
           onClick={() => history.push("/classrooms")}
@@ -73,43 +124,63 @@ export default function ClassroomListPage() {
           <CircularProgress />
         </Grid>
       )}
-      {getError && <Alert severity="error">Failed loading classrooms!</Alert>}
+      {getError && (
+        <Alert severity="error">Failed loading classroom detail!</Alert>
+      )}
       {!getLoading && !getError && (
         <>
-          {editing ? (
-            <TextField
-              value={classroom?.name}
-              required
-              onChange={(e) =>
-                setClassroom({
-                  ...classroom,
-                  name: e.target.value,
-                } as Classroom)
-              }
-            />
-          ) : (
-            <Typography variant="h2" className="mb-4">
-              {classroom?.name}
-            </Typography>
-          )}
+          <Grid item xs={12} className="mb-4">
+            {editing ? (
+              <TextField
+                value={classroom?.name}
+                required
+                onChange={(e) =>
+                  setClassroom({
+                    ...classroom,
+                    name: e.target.value,
+                  } as Classroom)
+                }
+              />
+            ) : (
+              <Typography variant="h2">{classroom?.name}</Typography>
+            )}
 
-          <Grid item xs={12}>
             {editing ? (
               <>
-                <Button onClick={handleEdit} startIcon={<Icon>save</Icon>}>
-                  Save
-                </Button>
-                <Button onClick={handleCancel} startIcon={<Icon>cancel</Icon>}>
-                  Cancel
-                </Button>
+                <IconButton onClick={handleEdit}>
+                  <Icon>save</Icon>
+                </IconButton>
+                <IconButton onClick={handleCancel}>
+                  <Icon>cancel</Icon>
+                </IconButton>
               </>
             ) : (
               <>
-                <Button onClick={handleEdit} startIcon={<Icon>edit</Icon>}>
-                  Edit
-                </Button>
+                <IconButton onClick={handleEdit}>
+                  <Icon>edit</Icon>
+                </IconButton>
               </>
             )}
+          </Grid>
+          <Grid item xs={12} className="mb-4">
+            {!getOngoingSessionLoading &&
+              (ongoingSession ? (
+                <Button
+                  disabled={deleteOngoingSessionLoading}
+                  onClick={handleDeleteOngoingSession}
+                >
+                  Stop Ongoing Session
+                </Button>
+              ) : (
+                <Button component={Link} to={`/classroom/${classroomId}/hub`}>
+                  Start New Session
+                </Button>
+              ))}
+          </Grid>
+          <Grid item xs={12}>
+            <Button disabled={deleteLoading || editing} onClick={handleDelete}>
+              Delete Classroom
+            </Button>
           </Grid>
         </>
       )}
