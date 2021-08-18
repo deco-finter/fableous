3f3f51721d25ec64c69080eb5703bf8c3cbf8541
Danny August Ramaputra <daystram@gmail.com>
2021-08-18 14:10:23 +1000

    refactor: fixed loading list

--- fableous-fe/src/containers/ClassroomListPage.tsx
+++ fableous-fe/src/containers/ClassroomListPage.tsx
@@ -74,81 +74,83 @@ export default function ClassroomListPage() {
         </Grid>
       )}
       {getError && <Alert severity="error">Failed loading classrooms!</Alert>}
-      <Grid container spacing={2}>
-        {classrooms?.data?.map((classroom) => (
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className="flex flex-col h-full">
-              <CardContent className="flex-grow">
-                <Typography variant="h5" component="h2">
-                  {classroom.name}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  component={Link}
-                  to={`/classroom/${classroom.id}`}
-                >
-                  View
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-        {creating ? (
-          <Grid item xs={12} sm={6} md={4}>
-            <Card className="flex flex-col h-full">
-              <CardContent className="flex-grow">
-                <TextField
-                  autoFocus
-                  placeholder="Classroom name"
-                  value={newClassroom?.name}
-                  required
-                  disabled={postLoading}
-                  onChange={(e) =>
-                    setNewClassroom({
-                      ...newClassroom,
-                      name: e.target.value,
-                    } as Classroom)
-                  }
-                />
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  disabled={postLoading}
-                  onClick={handleCreate}
-                >
-                  Create
-                </Button>
-                <Button
-                  size="small"
-                  disabled={postLoading}
-                  onClick={handleCancel}
-                >
-                  Cancel
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ) : (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            container
-            justifyContent="center"
-            alignItems="center"
-          >
-            <IconButton>
-              <Icon fontSize="large" onClick={handleCreate}>
-                add
-              </Icon>
-            </IconButton>
-          </Grid>
-        )}
-      </Grid>
+      {!getLoading && !getError && (
+        <Grid container spacing={2}>
+          {classrooms?.data?.map((classroom) => (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className="flex flex-col h-full">
+                <CardContent className="flex-grow">
+                  <Typography variant="h5" component="h2">
+                    {classroom.name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    component={Link}
+                    to={`/classroom/${classroom.id}`}
+                  >
+                    View
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+          {creating ? (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className="flex flex-col h-full">
+                <CardContent className="flex-grow">
+                  <TextField
+                    autoFocus
+                    placeholder="Classroom name"
+                    value={newClassroom?.name}
+                    required
+                    disabled={postLoading}
+                    onChange={(e) =>
+                      setNewClassroom({
+                        ...newClassroom,
+                        name: e.target.value,
+                      } as Classroom)
+                    }
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    disabled={postLoading}
+                    onClick={handleCreate}
+                  >
+                    Create
+                  </Button>
+                  <Button
+                    size="small"
+                    disabled={postLoading}
+                    onClick={handleCancel}
+                  >
+                    Cancel
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ) : (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              container
+              justifyContent="center"
+              alignItems="center"
+            >
+              <IconButton>
+                <Icon fontSize="large" onClick={handleCreate}>
+                  add
+                </Icon>
+              </IconButton>
+            </Grid>
+          )}
+        </Grid>
+      )}
     </Grid>
   );
 }
