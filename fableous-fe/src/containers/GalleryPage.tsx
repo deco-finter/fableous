9dd3bc91d65b1133b20cf4949f37f4d4351e59b7
wahyuadt <wahyuadt9@gmail.com>
2021-08-20 13:55:45 +1000

    feat: add gallery page

---
+++ fableous-fe/src/containers/GalleryPage.tsx
@@ -0,0 +1,45 @@
+import { Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default function HomePage() {
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      style={{ height: "80vh" }}
+    >
+      <Grid item>
+        <Typography variant="h1" className="text-center">
+          Fableous
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="subtitle1" className="text-center">
+          Collaborative Storyteller
+        </Typography>
+      </Grid>
+      <div className="mt-16">
+        <Button
+          size="large"
+          variant="outlined"
+          className="mx-4"
+          component={Link}
+          to="/login"
+        >
+          Teacher
+        </Button>
+        <Button
+          size="large"
+          variant="outlined"
+          className="mx-4"
+          component={Link}
+          to="/join"
+        >
+          Student
+        </Button>
+      </div>
+    </Grid>
+  );
+}
