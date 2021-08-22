0f7bf1e70442d26220eeb741ec2ad8aa3b0ab5f6
wahyuadt <wahyuadt9@gmail.com>
2021-08-22 17:32:23 +1000

    feat: create gallery page

--- fableous-fe/src/containers/GalleryPage.tsx
+++ fableous-fe/src/containers/GalleryPage.tsx
@@ -1,45 +1,66 @@
-import { Button, Grid, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import {
+  Button,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+  Typography,
+} from "@material-ui/core";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import useAxios from "axios-hooks";
+import { Alert } from "@material-ui/lab";
+import { restAPI } from "../Api";
+import { APIResponse, Story } from "../Data";
 
-export default function HomePage() {
+export default function GalleryPage() {
+  const { classroomId } = useParams<{ classroomId: string }>();
+  const [{ data: stories, loading: getLoading, error: getError }, executeGet] =
+    useAxios<APIResponse<Story[]>, APIResponse<undefined>>(
+      restAPI.gallery.getList(classroomId),
+      { manual: true }
+    );
+
+  useEffect(() => {
+    executeGet();
+  }, [executeGet]);
+
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
-      style={{ height: "80vh" }}
-    >
-      <Grid item>
-        <Typography variant="h1" className="text-center">
-          Fableous
-        </Typography>
+    <Grid container>
+      <Grid item xs={12} className="mb-4">
+        <Typography variant="h2">Gallery</Typography>
       </Grid>
-      <Grid item>
-        <Typography variant="subtitle1" className="text-center">
-          Collaborative Storyteller
-        </Typography>
-      </Grid>
-      <div className="mt-16">
-        <Button
-          size="large"
-          variant="outlined"
-          className="mx-4"
-          component={Link}
-          to="/login"
-        >
-          Teacher
-        </Button>
-        <Button
-          size="large"
-          variant="outlined"
-          className="mx-4"
-          component={Link}
-          to="/join"
-        >
-          Student
-        </Button>
-      </div>
+      {getLoading && (
+        <Grid container justifyContent="center">
+          <CircularProgress />
+        </Grid>
+      )}
+      {getError && <Alert severity="error">Failed loading Gallery!</Alert>}
+      {!getLoading && !getError && (
+        <Grid container spacing={2}>
+          {stories?.data?.map((story) => (
+            <Grid item xs={12} sm={6} md={4} key={story.title}>
+              <Card className="flex flex-col h-full">
+                <CardContent className="flex-grow">
+                  <Typography variant="h5" component="h2">
+                    {story.title}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    component={Link}
+                    to={`/classroom/${story.title}`}
+                  >
+                    View
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Grid>
   );
 }
