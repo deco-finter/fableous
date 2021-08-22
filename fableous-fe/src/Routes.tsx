03a916a9ac47221cf19115802a0712b0fd123b76
wahyuadt <wahyuadt9@gmail.com>
2021-08-22 17:32:05 +1000

    feat: add route to gallery

--- fableous-fe/src/Routes.tsx
+++ fableous-fe/src/Routes.tsx
@@ -6,6 +6,7 @@ import HubCanvasPage from "./containers/HubCanvasPage";
 import HomePage from "./containers/HomePage";
 import LoginPage from "./containers/LoginPage";
 import RegisterPage from "./containers/RegisterPage";
+import GalleryPage from "./containers/GalleryPage";
 import ClassroomDetailPage from "./containers/ClassroomDetailPage";
 import ClassroomListPage from "./containers/ClassroomListPage";
 import { AuthContext } from "./components/AuthProvider";
@@ -31,6 +32,7 @@ export default function Routes() {
       <PublicRoute path="/login" component={LoginPage} exact />
       <PublicRoute path="/register" component={RegisterPage} exact />
       <PrivateRoute path="/classroom" component={ClassroomListPage} exact />
+      <Route path="/gallery/:classroomId" component={GalleryPage} exact />
       <PrivateRoute
         path="/classroom/:classroomId"
         component={ClassroomDetailPage}
