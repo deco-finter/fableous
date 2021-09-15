b73b7d5b4ee0ebad5ab1fb2e38c878284666c3d6
Danny August Ramaputra <daystram@gmail.com>
2021-09-15 18:27:03 +1000

    refactor: updated gallery list page

--- fableous-fe/src/containers/GalleryPage.tsx
+++ fableous-fe/src/containers/GalleryPage.tsx
@@ -1,44 +1,36 @@
-import {
-  Button,
-  Grid,
-  Card,
-  CardActions,
-  CardContent,
-  CircularProgress,
-  Typography,
-} from "@material-ui/core";
-import { useEffect, useContext } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Grid, CircularProgress, Typography } from "@material-ui/core";
+import { useEffect, useContext, useState } from "react";
+import { useParams } from "react-router-dom";
 import useAxios from "axios-hooks";
 import { Alert } from "@material-ui/lab";
 import { restAPI } from "../api";
 import { APIResponse, Session } from "../data";
 import { AuthContext } from "../components/AuthProvider";
+import StoryItem from "../components/StoryItem";
 
 export default function GalleryPage() {
   const { classroomId } = useParams<{ classroomId: string }>();
+  const [, setTicker] = useState(0);
   const [, isAuthenticated, ,] = useContext(AuthContext);
-  const [{ data: stories, loading: getLoading, error: getError }, executeGet] =
+  const [{ data: sessions, loading: getLoading, error: getError }, executeGet] =
     useAxios<APIResponse<Session[]>, APIResponse<undefined>>(
       restAPI.session.getList(classroomId),
       { manual: true }
     );
-  const [{ loading: deleteLoading }, executeDelete] = useAxios<
-    APIResponse<string>,
-    APIResponse<undefined>
-  >(restAPI.classroom.delete(classroomId), { manual: true });
+
+  const handleDelete = (sessionId: string) => {
+    if (sessions) {
+      sessions.data = sessions?.data?.filter(
+        (session) => session.id !== sessionId
+      );
+      setTicker((prev) => prev + 1);
+    }
+  };
+
   useEffect(() => {
     executeGet();
   }, [executeGet]);
-  const handleDelete = (sessionId: string) => {
-    executeDelete({
-      url: restAPI.session.delete(classroomId, sessionId).url,
-    })
-      .then(() => {
-        executeGet();
-      })
-      .catch((error) => console.error(error));
-  };
+
   return (
     <Grid container>
       <Grid item xs={12} className="mb-4">
@@ -52,45 +44,14 @@ export default function GalleryPage() {
       {getError && <Alert severity="error">Failed loading Gallery!</Alert>}
       {!getLoading && !getError && (
         <Grid container spacing={2}>
-          {stories?.data?.map((story) => (
-            <Grid item xs={12} sm={6} md={4} key={story.title}>
-              <Card className="flex flex-col h-full">
-                <CardContent className="flex-grow">
-                  <Typography variant="h5" component="h2">
-                    {story.title}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  {isAuthenticated ? (
-                    <>
-                      <Button
-                        size="small"
-                        disabled={deleteLoading}
-                        onClick={() => handleDelete(story.id)}
-                      >
-                        Delete
-                      </Button>
-                      <Button
-                        size="small"
-                        component={Link}
-                        to={`/gallery/${classroomId}/${story.id}`}
-                      >
-                        View
-                      </Button>{" "}
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        size="small"
-                        component={Link}
-                        to={`/gallery/${classroomId}/${story.id}`}
-                      >
-                        View
-                      </Button>{" "}
-                    </>
-                  )}
-                </CardActions>
-              </Card>
+          {sessions?.data?.map((session) => (
+            <Grid item xs={12} sm={6} md={4} key={session.id}>
+              <StoryItem
+                session={session}
+                classroomId={classroomId}
+                editable={isAuthenticated}
+                onDelete={() => handleDelete(session.id)}
+              />
             </Grid>
           ))}
         </Grid>
